a8cf4144b8a7133af1b46c7b64eba0647fb13e40
ealimov <ealimov@htmlbox.net>
2024-12-21 21:03:02 +0200

    feat(components/card): add component and styles

---
+++ src/components/card/card.jsx
@@ -0,0 +1,11 @@
+const Card = () => {
+	return (
+		<div className="card">
+			<h3 className="card__title">Example title</h3>
+
+			<p className="card__description">Lorem ipsum dolor sit </p>
+		</div>	
+	)
+}
+
+export default Card;
