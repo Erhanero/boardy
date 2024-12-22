2313b9afcb2ffdc58f92498ed5c00b8b7cb1ff5a
ealimov <ealimov@htmlbox.net>
2024-12-23 00:34:23 +0200

    feat(globals): add plugins

--- src/components/board-lists/board-lists.jsx
+++ src/components/board-lists/board-lists.jsx
@@ -7,7 +7,7 @@ import Stack from '@/components/stack/stack';
 const BoardLists = () => {
     return (
         <div className="board-lists">
-            <Stack columnGap="30" justifyContent="center" style={{overflow: "auto"}}>
+            <Stack columnGap="20" style={{ overflow: "auto", padding: "5px", margin: "-5px"}}>
                 <BoardList title="To do" />
                 <BoardList title="In progress" />
                 <BoardList title="Completed" />
