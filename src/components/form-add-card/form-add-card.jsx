f1ee99a62ef42450331d0c9bc3b1da1332630771
ealimov <ealimov@htmlbox.net>
2024-12-23 00:31:49 +0200

    feat(components/form-add-card): add component

--- src/components/form-add-card/form-add-card.jsx
+++ src/components/form-add-card/form-add-card.jsx
@@ -7,47 +7,58 @@ import { useForm } from 'react-hook-form';
  * Internal dependencies.
  */
 import InputField from '@/components/input-field/input-field';
-import Button from '@/components/button/button';
+import Form from '@/components/form/form';
 
 const FormAddCard = () => {
 	const {
 		handleSubmit,
 		register,
-		// formState: { errors },
+		formState: { errors },
 	} = useForm()
 
 	const onSubmit = (data) => {
 		console.log(data);
+		console.log(errors);
 	}
-	
+
+	const fields = [
+		{
+			id: 'title',
+			name: 'title',
+			label: 'Title',
+			validation: { required: 'Title is required' },
+			type: 'text',
+		},
+		{
+			id: 'description',
+			name: 'description',
+			label: 'Description',
+			type: 'textarea',
+		},
+		{
+			id: 'label',
+			name: 'label',
+			label: 'Label',
+			type: 'text',
+		},
+	];
+
 	return (
-		<div className="form">
-			<form action="?" method="post" onSubmit={handleSubmit(onSubmit)}>
-				<div className="form__body">
-					<div className="form__controls">
-						<label htmlFor="title" className="form__label">Title</label>
+		<Form onSubmit={handleSubmit(onSubmit)} submitBtnText="Add">
+			{fields.map(({ id, name, label, validation, type }) => (
+				<div className="form__controls" key={id}>
+					<label htmlFor={id} className="form__label">
+						{label}
+					</label>
 
-						<InputField {...register("title", { required: true })} name="title" id="title" />
-					</div>
+					<InputField {...register(name, validation)} type={type} name={name} id={id} />
 
-					<div className="form__controls">
-						<label htmlFor="description" className="form__label">Description</label>
-
-						<InputField {...register("description")} type="textarea" name="description" id="description" />
-					</div>
-
-					<div className="form__controls">
-						<label htmlFor="label" className="form__label">Label</label>
-
-						<InputField {...register("label")} name="label" id="label"/>
-					</div>
+					{errors[name]?.type === 'required' && (
+						<span role="form__error">{errors[name]?.message}</span>
+					)}
 				</div>
-                        
-				<div className="form__actions">
-					<Button type="submit" variant="blue">Add</Button>					
-				</div>
-			</form>
-		</div>
+			))}
+		</Form >
 	);
 }
 
