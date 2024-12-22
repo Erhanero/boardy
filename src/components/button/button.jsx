0669326825ddd1cdc9c0b6a4deaa6beb463680a7
ealimov <ealimov@htmlbox.net>
2024-12-22 08:43:23 +0200

    feat(components/button): add styles

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -6,6 +6,7 @@ import classNames from 'classnames';
 const Button = (props) => {
     const {
         as: Component = 'button',
+        variant,
         href,
         onClick,
         className,
@@ -16,7 +17,7 @@ const Button = (props) => {
         <Component
             onClick={onClick}
             href={href}
-            className={classNames("btn", className)}
+            className={classNames("button", className, variant && `button--${variant}`)}
         >
             {children}
         </Component>
