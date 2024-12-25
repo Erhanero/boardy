7c2a0d60ce41afc9c486dfba337bd55dbe493d6b
ealimov <ealimov@htmlbox.net>
2024-12-25 22:27:38 +0200

    refactor(components/board): change name and layout

---
+++ src/components/board/board.jsx
@@ -0,0 +1,24 @@
+/**
+ * Internal dependencies.
+ */
+import BoardList from '@/components/board-list/board-list';
+import Stack from '@/components/stack/stack';
+import EditableText from '@/components/editable-text/editable-text';
+
+const Board = () => {
+    return (
+        <div className="board">
+            <div className="board__head">
+                <EditableText initialText="My Board" fontSize="22px" />                  
+            </div>
+
+                <Stack className="board__inner" alignItems="flex-start" columnGap="20" >
+                    <BoardList title="To do" />
+                    <BoardList title="In progress" />
+                    <BoardList title="Completed" />
+                </Stack>
+        </div>
+    );
+};
+
+export default Board;
