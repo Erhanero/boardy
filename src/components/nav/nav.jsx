190e03826f00832060cbc2c6e5a2338f30a5167a
ealimov <ealimov@htmlbox.net>
2024-12-26 19:54:31 +0200

    feat(components/nav): add navlink for routes

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,9 +1,14 @@
+/**
+ * External dependencies.
+ */
+import { NavLink } from 'react-router-dom';
+
 const Nav = () => {
 	return (
 		<nav className="nav">
 			<ul>
 				<li>
-					<a href="#">
+					<NavLink to="/" className="nav__link">
 						<div className="nav__icon">
 							<svg width="24" viewBox="0 0 24 24">
 								<path fill="none" d="M0 0h24v24H0z">
@@ -14,11 +19,11 @@ const Nav = () => {
 						</div>
 
 						<span className="nav__text">Boards</span>
-					</a>
+					</NavLink>
 				</li>
 
 				<li>
-					<a href="#">
+					<NavLink to="/create-board" className="nav__link">
 						<div className="nav__icon">
 							<svg width="24" viewBox="0 0 512 512">
 								<path d="M464 96h-192l-64-64h-160C21.5 32 0 53.5 0 80v352C0 458.5 21.5 480 48 480h416c26.5 0 48-21.5 48-48v-288C512 117.5 490.5 96 464 96zM336 311.1h-56v56C279.1 381.3 269.3 392 256 392c-13.27 0-23.1-10.74-23.1-23.1V311.1H175.1C162.7 311.1 152 301.3 152 288c0-13.26 10.74-23.1 23.1-23.1h56V207.1C232 194.7 242.7 184 256 184s23.1 10.74 23.1 23.1V264h56C349.3 264 360 274.7 360 288S349.3 311.1 336 311.1z">
@@ -26,12 +31,12 @@ const Nav = () => {
 							</svg>
 						</div>
 
-						<span className="nav__text">Add a new board</span>
-					</a>
+						<span className="nav__text">Create board</span>
+					</NavLink>
 				</li>
 
 				<li>
-					<a href="#">
+					<NavLink to="/login" className="nav__link">
 						<span className="nav__icon">
 							<svg width="24" viewBox="0 0 24 24">
 								<path fill="none" d="M0 0h24v24H0z">
@@ -42,11 +47,11 @@ const Nav = () => {
 						</span>
 
 						<span className="nav__text">Login</span>
-					</a>
+					</NavLink>
 				</li>
 
 				<li>
-					<a href="#">
+					<NavLink to="/register" className="nav__link">
 						<span className="nav__icon">
 							<svg width="24" viewBox="0 0 24 24">
 								<path d="M0 0h24v24H0z" fill="none">
@@ -57,7 +62,7 @@ const Nav = () => {
 						</span>
 
 						<span className="nav__text">Register</span>
-					</a>
+					</NavLink>
 				</li>
 			</ul>
 		</nav>
