93d89b37df92da34e86511efa4a5955d0a680a5d
ealimov <ealimov@htmlbox.net>
2024-12-26 19:50:15 +0200

    feat(layout/main-layout): add layout

---
+++ src/components/main-content/main-content.jsx
@@ -0,0 +1,9 @@
+const MainContent = ({children}) => {
+	return (
+		<main className="main-content">
+			{children}
+		</main>
+	)
+}
+
+export default MainContent;
---
+++ src/layouts/main-layout.jsx
@@ -0,0 +1,21 @@
+/**
+ * External dependencies.
+ */
+import { Outlet } from 'react-router-dom';
+import Sidebar from '@/components/sidebar/sidebar';
+import Stack from '@/components/stack/stack';
+import MainContent from '@/components/main-content/main-content';
+
+const MainLayout = () => {
+	return (
+		<Stack>
+			<Sidebar />
+
+			<MainContent>				
+				<Outlet />
+			</MainContent>
+		</Stack>
+	);
+}
+
+export default MainLayout;
