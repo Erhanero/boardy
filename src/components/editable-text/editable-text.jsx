38e55d6d138481c305df72751c68dabc4d32947d
ealimov <ealimov@htmlbox.net>
2024-12-26 19:50:42 +0200

    feat(components/editable-text): add icon

--- src/components/editable-text/editable-text.jsx
+++ src/components/editable-text/editable-text.jsx
@@ -4,16 +4,16 @@
 import { useState, useRef, useEffect } from 'react';
 import classNames from 'classnames';
 
-const EditableText = ({initialText, fontSize = '16px', className}) => {
+const EditableText = ({ initialText, fontSize = '16px', className }) => {
 	const [text, setText] = useState(initialText);
-	const textareaRef = useRef(null);	
+	const textareaRef = useRef(null);
 	const [textHeight, setTextHeight] = useState('auto');
 
 	useEffect(() => {
-		if (textareaRef.current) {	
-			setTextHeight(textareaRef.current.scrollHeight)		 
+		if (textareaRef.current) {
+			setTextHeight(textareaRef.current.scrollHeight)
 		}
-	  }, [text]);
+	}, [text]);
 
 	/**
 	 * Handle text change.
@@ -25,22 +25,31 @@ const EditableText = ({initialText, fontSize = '16px', className}) => {
 		if (e.target.value == '') {
 			return;
 		}
-		
-		setText(e.target.value);	
+
+		setText(e.target.value);
 	}
 
 	return (
 		<div
 			className={classNames("editable-text", className)}
 			style={{ height: textHeight, "--font-size": `${fontSize}` }}
-		>			
-			<h3>{ text }</h3>
-		
+		>
+			<h3>{text}</h3>
+
 			<textarea
 				value={text}
 				onChange={handleTextchange}
 				ref={textareaRef}
 			>{text}</textarea>
+
+			<span className="editable__icon">
+				<svg  viewBox="0 0 24 24">
+					<path d="M0 0h24v24H0z" fill="none">
+					</path>
+					<path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04a.996.996 0 0 0 0-1.41l-2.34-2.34a.996.996 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z">
+					</path>
+				</svg>
+			</span>
 		</div>
 	)
 }
