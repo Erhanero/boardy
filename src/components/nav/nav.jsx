afd8610f59c3f2ffe330ab6a74b395a0ca7a6ffc
ealimov <ealimov@htmlbox.net>
2024-12-28 14:05:02 +0200

    feat(components/nav): add logout link

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -64,6 +64,22 @@ const Nav = () => {
 						<span className="nav__text">Register</span>
 					</NavLink>
 				</li>
+
+				<li>
+					<NavLink to="/logout" className="nav__link">
+						<span className="nav__icon">
+							<svg width="24" viewBox="0 0 24 24">
+								<path d="M0 0h24v24H0z" fill="none">
+								</path>
+								<path d="m17 7-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z">
+								</path>
+							</svg>
+						</span>
+
+						<span className="nav__text">Logout</span>
+					</NavLink>
+				</li>
+
 			</ul>
 		</nav>
 	)
