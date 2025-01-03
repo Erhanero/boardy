33926cc4fadb315e1fbd5fbf217b46f55b9fab73
ealimov <ealimov@htmlbox.net>
2025-01-03 17:22:03 +0200

    feat(components/cards): load cards from backend

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -10,10 +10,21 @@ import Card from '@/components/card/card';
 import Stack from '@/components/stack/stack';
 import Button from '@/components/button/button';
 import Modal from '@/components/modal/modal';
-import FormAddCard from '@/components/form-add-card/form-add-card';
+import FormCard from '@/components/form-card/form-card';
+import useCards from '@/hooks/cards/use-cards';
+import LoadingSpinner from '@/components/loading-spinner/loading-spinner';
 
-const Cards = () => {
+const Cards = ({ listId, boardId }) => {
+    const { cards, isLoading } = useCards(listId);
     const [showModal, setShowModal] = useState(false);
+    const [isEditMode, setIsEditMode] = useState(false);
+    const [selectedCard, setSelectedCard] = useState(null);
+
+    const cardClickHandler = (cardData) => {
+        setIsEditMode(true);
+        setShowModal(true);
+        setSelectedCard(cardData);
+    }
 
     /**
      * Handle close modal.
@@ -22,6 +33,8 @@ const Cards = () => {
      */
     const handleCloseModal = () => {
         setShowModal(false);
+        setIsEditMode(false);
+        setSelectedCard(null);
     };
 
     /**
@@ -35,21 +48,46 @@ const Cards = () => {
         setShowModal(true);
     };
 
+    if (isLoading) {
+        return <LoadingSpinner width="60" />;
+    }
+
     return (
         <div className="cards">
             <div className="cards__inner">
                 <Stack direction="column" alignItems="normal" rowGap="12">
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
+                    {cards.map(card => {
+                        return <Card
+                            key={card.id}
+                            id={card.id}
+                            title={card.title}
+                            description={card.description}
+                            label={card.label}
+                            onClick={cardClickHandler}
+                        />;
+                    })}
                 </Stack>
             </div>
 
             <Button className="cards__button" onClick={handleOpenModal}>+ Add a card</Button>
 
-            <Modal isOpen={showModal} title="Create new card" onClose={handleCloseModal}>
-                <FormAddCard />
+            <Modal isOpen={showModal} title={isEditMode ? "Edit card" : "Create new card"} onClose={handleCloseModal}>
+                {isEditMode ? (
+                    <FormCard
+                        cardData={selectedCard}
+                        mode="edit"
+                        listId={listId}
+                        boardId={boardId}
+                        onSuccess={handleCloseModal}
+                    />
+
+                ) : (
+                    <FormCard
+                        listId={listId}
+                        boardId={boardId}
+                        onSuccess={handleCloseModal}
+                    />
+                )}
             </Modal>
         </div>
     );
