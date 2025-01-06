23a57ef64ad2a77dedac96dea2f0be3ea319565e
ealimov <ealimov@htmlbox.net>
2025-01-06 22:48:24 +0200

    feat(app): add react-hot-toast for notifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
 /**
+ * External dependencies.
+ */
+import { Toaster } from 'react-hot-toast';
+
+/**
 * Internal dependencies.
 */
 import Router from '@/router';
 import { AuthProvider } from '@/contexts/auth';
 
 function App() {
-    
+
     return (
-        <AuthProvider>
-            <Router />
-        </AuthProvider>
+        <>
+            <AuthProvider>
+                <Router />
+            </AuthProvider>
+
+            <Toaster
+                position="top-right"
+                toastOptions={{
+                    duration: 3000,
+                    style: {
+                        background: '#056dd4',
+                        color: '#fff',
+                    },
+                }}
+            />
+        </>
     );
 }
 
